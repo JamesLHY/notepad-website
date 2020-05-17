94a396451183b92ae8ac02e8764fee2f908b6c09
James <847798512@qq.com>
2020-05-17 15:36:33 +0800

    deploy

---
+++ precache-manifest.a47019479b7a2a305036f1e59d8d7bd5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "581847c7042905bf949e",
+    "url": "/xiaobai-notepad-vue-version--website/css/app.fd106623.css"
+  },
+  {
+    "revision": "4d1dbf7d51fe7def6e96c28ca5a23e90",
+    "url": "/xiaobai-notepad-vue-version--website/index.html"
+  },
+  {
+    "revision": "581847c7042905bf949e",
+    "url": "/xiaobai-notepad-vue-version--website/js/app.86fcbaac.js"
+  },
+  {
+    "revision": "40120ee2bdca46140b42",
+    "url": "/xiaobai-notepad-vue-version--website/js/chunk-vendors.cbb1b2ee.js"
+  },
+  {
+    "revision": "66f5a7992048fecbf85674ce58551867",
+    "url": "/xiaobai-notepad-vue-version--website/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/xiaobai-notepad-vue-version--website/robots.txt"
+  }
+]);
